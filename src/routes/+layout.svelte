2eedcba6e1002a30d1ad6b8425dc382ee34c7b09
TNThung <hung11112002@gmail.com>
2023-06-08 09:15:53 +0800

    [Add] custom title bar & rounded window

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,41 @@
   import "virtual:uno.css";
   import "../assets/fontawesome/scss/solid.scss";
   import "../assets/fontawesome/scss/fontawesome.scss";
+
+  import dark from "../store/darkmode";
+
+  import Titlebar from "./Titlebar.svelte";
 </script>
 
 
-<div class="bgc-000 fixed inset-0">
-  <slot/>
+<div class="page flex flex-col fixed inset-0 bdr-5 of-hidden bgc-0000" class:dark={$dark}>
+  <Titlebar/>
+  <main class="flex-1">
+    <slot/>
+  </main>
 </div>
+
+
+<style lang="scss" global>
+  div.page {
+    --txt: #031107;
+    --bgc: #d9f7e2;
+    --pri: #1d913f;
+    --sec: #f6fdf9;
+    --acc: #187b36;
+
+    &.dark {
+      --txt: #f6f9f5;
+      --bgc: #000000;
+      --sec: #0a100a;
+    }
+
+    font-family: Arial, Helvetica, sans-serif;
+
+    color: var(--txt);
+
+    main {
+      background-color: var(--bgc);
+    }
+  }
+</style>
---
+++ src/routes/Titlebar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { appWindow } from "@tauri-apps/api/window";
+</script>
+
+
+<div class="flex h-20px">
+  <span data-tauri-drag-region class="block lh-20px select-none text-13px font-900 ml-10px flex-1">AutoMaster</span>
+  <button on:click={appWindow.minimize} class="reset"><i class="fa-solid fa-minus"/></button>
+  <button on:click={appWindow.close   } class="reset"><i class="fa-solid fa-xmark"/></button>
+</div>
+
+
+<style lang="scss">
+  div {
+    color: #0009;
+    background-color: var(--acc);
+
+    button {
+      height: 100%;
+      display: grid;
+      aspect-ratio: 1.2/1;
+      place-content: center;
+
+      &:hover {
+        background-color: var(--pri);
+      }
+    }
+  }
+</style>
